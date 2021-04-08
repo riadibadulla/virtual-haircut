a2f4325bc9972d9056f9c8658ff8756d6ab1e5bc
Riad <riadibadulla@gmail.com>
2021-04-08 01:38:43 +0100

    Face landmarks identified and displayed. image has to be deleted manually

---
+++ virtualHaircut/facerecognition/main_ai.py
@@ -0,0 +1,41 @@
+import face_recognition
+import PIL.Image
+import PIL.ImageDraw
+import os
+
+
+
+
+def run_landmark_detection():
+    print(os.listdir("facerecognition/static/images/")[-1])
+    IMAGE_DIR = "facerecognition/static/images/"+os.listdir("facerecognition/static/images/")[-1]
+
+
+    # Load the jpg file into a numpy array
+    image = face_recognition.load_image_file(IMAGE_DIR)
+
+    # Find all facial features in all the faces in the image
+    face_landmarks_list = face_recognition.face_landmarks(image)
+
+    number_of_faces = len(face_landmarks_list)
+    print("I found {} face(s) in this photograph.".format(number_of_faces))
+
+    # Load the image into a Python Image Library object so that we can draw on top of it and display it
+    pil_image = PIL.Image.fromarray(image)
+
+    # Create a PIL drawing object to be able to draw lines later
+    draw = PIL.ImageDraw.Draw(pil_image)
+
+    # Loop over each face
+    for face_landmarks in face_landmarks_list:
+
+        # Loop over each facial feature (eye, nose, mouth, lips, etc)
+        for name, list_of_points in face_landmarks.items():
+            # Print the location of each facial feature in this image
+            print("The {} in this face has the following points: {}".format(name, list_of_points))
+
+            # Let's trace out each facial feature in the image with a line!
+            draw.line(list_of_points, fill="red", width=2)
+
+    pil_image.save(IMAGE_DIR)
+
--- virtualHaircut/facerecognition/views.py
+++ virtualHaircut/facerecognition/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from . import main_ai
 
 def home(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
+        filename = fs.save('facerecognition/static/images/1.jpg', myfile)
+        uploaded_file_url = fs.url('images/'+myfile.name)
+        print(uploaded_file_url)
+        main_ai.run_landmark_detection()
         return render(request, 'home.html', {
             'uploaded_file_url': uploaded_file_url
         })
